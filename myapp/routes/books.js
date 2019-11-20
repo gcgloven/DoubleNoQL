9e05d6f712d28cea1465efd0626dd1a621ce1cc0
hi <huanxuan_oh@mymail.sutd.edu.sg>
2019-11-20 11:31:25 +0800

    mongodb data shows up in reviews page

--- myapp/routes/books.js
+++ myapp/routes/books.js
@@ -3,10 +3,13 @@ var mongoose = require("mongoose"),
   Product = require("./../models/bookModel"),
   Product = mongoose.model("Books");
 
-mongoose.connect("mongodb://ec2-52-221-249-173.ap-southeast-1.compute.amazonaws.com/dbproj", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-});
+mongoose.connect(
+  "mongodb://ec2-52-221-249-173.ap-southeast-1.compute.amazonaws.com/dbproj",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }
+);
 
 mongoose.connection.on("error", function(error) {
   console.log("Connection error: ", error);
@@ -14,48 +17,59 @@ mongoose.connection.on("error", function(error) {
 
 var express = require("express");
 var products = express.Router();
+var bodyParser = require("body-parser");
+products.use(bodyParser.json());
+products.use(bodyParser.urlencoded({ extended: true }));
 
-products.get('/books/:page', function(req, res, next) {
-  var perPage = 9
-  var page = req.params.page || 1
+products.get("/books/:page", function(req, res, next) {
+  var perPage = 9;
+  var page = req.params.page || 1;
+  // if (req.query.sort == "price") {
+  //   var sorter = "-price";
+  // } else {
+  //   var sorter = "asin";
+  // }
+  // const sorter1 = req.body.sortBy;
+  // console.log("sorter: " + req.body.sortBy);
 
-  Product
-      .find({})
-      .skip((perPage * page) - perPage)
-      .limit(9)
-      .exec(function(err, products) {
-          Product.count().exec(function(err, count) {
-              if (err) return next(err)
-              res.render('books', {
-                  books: products,
-                  current: page,
-                  pages: Math.ceil(count/perPage)
-              });
-              console.log(products);
-          });
+  Product.find({})
+    //.sort(sorter1)
+    .skip(perPage * page - perPage)
+    .limit(9)
+    .exec(function(err, products) {
+      Product.count().exec(function(err, count) {
+        if (err) return next(err);
+        res.render("books", {
+          books: products,
+          current: page,
+          pages: Math.ceil(count / perPage),
+          query: req.query,
+          url: req.originalUrl
+        });
+        console.log(products);
       });
+    });
 });
 
+products.get("/books", function(req, res, next) {
+  var perPage = 9;
+  var page = req.params.page || 1;
 
-products.get('/books', function(req, res, next) {
-  var perPage = 9
-  var page = req.params.page || 1
-
-  Product
-      .find({})
-      .skip((perPage * page) - perPage)
-      .limit(9)
-      .exec(function(err, products) {
-          Product.count().exec(function(err, count) {
-              if (err) return next(err)
-              res.render('books', {
-                  books: products,
-                  current: page,
-                  pages: Math.ceil(count/perPage)
-              });
-              console.log(products);
-          });
+  Product.find({})
+    //.sort("-price")
+    .skip(perPage * page - perPage)
+    .limit(9)
+    .exec(function(err, products) {
+      Product.count().exec(function(err, count) {
+        if (err) return next(err);
+        res.render("books", {
+          books: products,
+          current: page,
+          pages: Math.ceil(count / perPage)
+        });
+        console.log(products);
       });
+    });
 });
 
 module.exports = products;
--- myapp/routes/reviews.js
+++ myapp/routes/reviews.js
@@ -4,18 +4,23 @@ var mongoose = require("mongoose"),
   Product = mongoose.model("Books"),
   Log = require("./../models/logModel"),
   Log = mongoose.model("Logs");
-  mongoose.connect("mongodb://ec2-52-221-249-173.ap-southeast-1.compute.amazonaws.com/dbproj", {
+mongoose.connect(
+  "mongodb://ec2-52-221-249-173.ap-southeast-1.compute.amazonaws.com/dbproj",
+  {
     useUnifiedTopology: true,
     useNewUrlParser: true
-});
+  }
+);
 
 mongoose.connection.on("error", function(error) {
   console.log("Connection error: ", error);
 });
 
-function getBookTitle(asin) {
-  var bookTitle = Product.find({ asin: asin });
-  return bookTitle.title;
+function getData(q) {
+  var query = Product.find(q, " -_id", function(err) {
+    if (err) throw err;
+  });
+  return query;
 }
 
 var connection = require("./../db/bookReviewsDb");
@@ -32,21 +37,20 @@ reviews.get("/reviews", function(req, res, next) {
 
       //post log information into mongodb database
       var newLog = new Log({
-          asin: asin,
-          request: 'Book Review',
-          date: new Date() + "",
-          status: 'fail'
+        asin: asin,
+        request: "Book Review",
+        date: new Date() + "",
+        status: "fail"
       });
-      newLog.save(function (err) {
-          //save done
-          if (err) {
-              console.log(err);
-              status: err
-              process.exit();
-          }
-          console.log('Log Saved')
+      newLog.save(function(err) {
+        //save done
+        if (err) {
+          console.log(err);
+          status: err;
+          process.exit();
+        }
+        console.log("Log Saved");
       });
-
     } else {
       console.log("reviews : ", result);
       res.render("reviews", {
@@ -56,37 +60,37 @@ reviews.get("/reviews", function(req, res, next) {
 
       //post log information into mongodb database
       var newLog = new Log({
-          asin: asin,
-          request: 'Book Review',
-          date: new Date() + "",
-          status: 'success'
+        asin: asin,
+        request: "Book Review",
+        date: new Date() + "",
+        status: "success"
       });
-      newLog.save(function (err) {
-          //save done
-          if (err) {
-              console.log(err);
-              status: err
-              process.exit();
-          }
-          console.log('Log Saved')
+      newLog.save(function(err) {
+        //save done
+        if (err) {
+          console.log(err);
+          status: err;
+          process.exit();
+        }
+        console.log("Log Saved");
       });
     }
   });
 
   //post log information into mongodb database
   var newLog = new Log({
-      asin: 'Null',
-      request: 'Book Review',
-      date: new Date() + ""
+    asin: "Null",
+    request: "Book Review",
+    date: new Date() + ""
   });
-  newLog.save(function (err) {
-      //save done
-      if (err) {
-          console.log(err);
-          status: err
-          process.exit();
-      }
-      console.log('Log Saved')
+  newLog.save(function(err) {
+    //save done
+    if (err) {
+      console.log(err);
+      status: err;
+      process.exit();
+    }
+    console.log("Log Saved");
   });
 });
 
@@ -94,55 +98,55 @@ reviews.get("/reviews/:asin", function(req, res, next) {
   var asin = req.params.asin;
   var q = "Select * from reviews where asin = '" + asin + "'";
 
-  connection.query(q, function(err, result) {
+  connection.query(q, async function(err, result) {
     if (err) {
       console.log("error: ", err);
       res.send(err);
 
       //post log information into mongodb database
       var newLog = new Log({
-          asin: asin,
-          request: 'Book Review',
-          date: new Date() + "",
-          status: 'fail'
+        asin: asin,
+        request: "Book Review",
+        date: new Date() + "",
+        status: "fail"
       });
-      newLog.save(function (err) {
-          //save done
-          if (err) {
-              console.log(err);
-              status: err
-              process.exit();
-          }
-          console.log('Log Saved')
+      newLog.save(function(err) {
+        //save done
+        if (err) {
+          console.log(err);
+          status: err;
+          process.exit();
+        }
+        console.log("Log Saved");
       });
-
     } else {
       console.log("reviews : ", result);
+      let mongoResult = await getData({ asin: asin });
+      let arrValues = Object.values(mongoResult);
+      console.log("result: " + arrValues[0]);
       res.render("reviews", {
-        title: getBookTitle(req.params.asin),
+        mongo: arrValues[0],
         reviews: result
       });
 
       //post log information into mongodb database
       var newLog = new Log({
-          asin: asin,
-          request: 'Book Review',
-          date: new Date() + "",
-          status: 'success'
+        asin: asin,
+        request: "Book Review",
+        date: new Date() + "",
+        status: "success"
       });
-      newLog.save(function (err) {
-          //save done
-          if (err) {
-              console.log(err);
-              status: err
-              process.exit();
-          }
-          console.log('Log Saved')
+      newLog.save(function(err) {
+        //save done
+        if (err) {
+          console.log(err);
+          status: err;
+          process.exit();
+        }
+        console.log("Log Saved");
       });
     }
   });
 });
-
-
 
 module.exports = reviews;
