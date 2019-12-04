511207fa8945e75dfbc6e217c11a2172c40a0e35
Valentine Zwe W. N <zwewint_naing@hotmail.com>
2019-12-04 17:30:54 +0800

    Frontend - Functional Search
    Fixed an error where pressing Home again will cause a 404 error.

--- myapp/app.js
+++ myapp/app.js
@@ -27,7 +27,7 @@ app.use("/users", usersRouter);
 app.use("/reviews", reviewsRouter);
 app.use("/books", booksRouter);
 app.use("/products", productsRouter);
-app.use("/", searchRouter);
+app.use("/search", searchRouter);
 app.use("/addbook", addnewbookRouter);
 
 app.use(express.static(path.join(__dirname, "public")));
