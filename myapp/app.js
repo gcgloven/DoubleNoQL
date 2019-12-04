4c097753f1807ebc7c1d7c88d2e199b04914eb17
Valentine Zwe W. N <zwewint_naing@hotmail.com>
2019-12-04 17:25:09 +0800

    UI - Working Navigation Links

--- myapp/app.js
+++ myapp/app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
+app.use("/home", indexRouter);
 app.use("/users", usersRouter);
 app.use("/reviews", reviewsRouter);
 app.use("/books", booksRouter);
